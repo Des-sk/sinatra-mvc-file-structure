c18f8bd2903ae5fadf7d2e5551129f35cb03c03d
Des-sk <marquezdecardona@gmail.com>
2021-02-10 00:45:06 -0400

    commit tree 587c1cf50be446e4ad0c1999ef919af7593bfb0b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,19 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  class Dog
+    attr_accessor :name, :breed, :age
+    DOGS = []
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+      DOGS << self
+    end
+  
+    def self.all
+      DOGS
+    end
+  
+  end
 end
